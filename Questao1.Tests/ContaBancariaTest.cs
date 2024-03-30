ffdb6485254399624b307024a15f67de6c9bba5d
Gaspar <gaspari.rodrigo@gmail.com>
2024-03-30 15:39:52 -0300

    The Resolution for Question 1

--- Questao1.Tests/ContaBancariaTest.cs
+++ Questao1.Tests/ContaBancariaTest.cs
@@ -1,13 +1,120 @@
+using System;
+
 namespace Questao1.Tests
 {
     public class ContaBancariaTest
     {
         [Fact]
-        public void ContaBancaria_IsConta_InputNumeroTitularDepositoInicial_OK()
+        public void ContaBancaria_WithInitialDeposit_ShowingOk()
         {
-            ContaBancaria conta = new ContaBancaria(5447, "Milton Gonçalves", 350.00);
+            //Arrange 
+            ContaBancaria conta = new(5447, "Milton Gonçalves", 350.00);
 
-            Assert.NotNull(conta);
+            //Act
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5447, Titular: Milton Gonçalves, Saldo: $ 350.00", result);
+        }
+
+
+        [Fact]
+        public void ContaBancaria_WithInitialDeposit_Deposit200_BalanceShowing550()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5447, "Milton Gonçalves", 350.00);
+
+            //Act
+            conta.Deposito(200); 
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5447, Titular: Milton Gonçalves, Saldo: $ 550.00", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithInitialDeposit_Deposit200_Withdraw199_BalanceShowing347_50()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5447, "Milton Gonçalves", 350.00);
+
+            //Act
+            conta.Deposito(200);
+            conta.Saque(199);
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5447, Titular: Milton Gonçalves, Saldo: $ 347.50", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithoutInitialDeposit_BalanceShowingZero()
+        { 
+            //Arrange 
+            ContaBancaria conta = new(5139, "Elza Soares");
+
+            //Act
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5139, Titular: Elza Soares, Saldo: $ 0.00", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithoutInitialDeposit_Deposit300_BalanceShowingOK()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5139, "Elza Soares");
+
+            //Act
+            conta.Deposito(300); 
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5139, Titular: Elza Soares, Saldo: $ 300.00", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithoutInitialDeposit_Deposit300_Withdraw298_BalanceShowingOK()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5139, "Elza Soares");
+
+            //Act
+            conta.Deposito(300);
+            conta.Saque(298);
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5139, Titular: Elza Soares, Saldo: $ -1.50", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithoutInitialDeposit_UpdateHolderName()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5139, "Elza Soares");
+
+            //Act
+            conta.AlterarNomeTitularConta("Elza Soares de Alcantara"); 
+            var result = conta.ToString();
+
+            //Assert
+            Assert.Equal("Conta 5139, Titular: Elza Soares de Alcantara, Saldo: $ 0.00", result);
+        }
+
+        [Fact]
+        public void ContaBancaria_WithoutInitialDeposit_WithdrawBiggerThanTheLimit()
+        {
+            //Arrange 
+            ContaBancaria conta = new(5139, "Elza Soares");
+
+            //Act
+            void act() => conta.Saque(1001);
+
+            //Assert
+            ArgumentException exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("Impossível realizar o saque acima do limite da conta.", exception.Message);
         }
     }
 }
--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -3,27 +3,49 @@ using System.Globalization;
 
 namespace Questao1
 {
-        public class ContaBancaria {
+    public class ContaBancaria
+    {
+        private int numero;
+        private string titular;
+        private double saldo;
+        private readonly double taxaDeSaque = 3.5;
+        private readonly double limiteChequeEspecial = 1000; //Não permite que o saque seja feito livremente.
 
         public ContaBancaria(int numero, string titular, double depositoInicial)
         {
-            throw new NotImplementedException();
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo += depositoInicial; 
         }
 
         public ContaBancaria(int numero, string titular)
         {
-            throw new NotImplementedException();
+            this.numero = numero;
+            this.titular = titular;
         }
 
-        public void Deposito(double quantia) 
+        public void Deposito(double quantia)
         {
-            throw new NotImplementedException();
+            this.saldo += quantia; 
         }
 
         public void Saque(double quantia)
         {
-            throw new NotImplementedException();
+            if ((this.saldo - quantia) < (limiteChequeEspecial * -1))
+                throw new ArgumentException("Impossível realizar o saque acima do limite da conta."); 
+
+            this.saldo -= taxaDeSaque;
+            this.saldo -= quantia;  
         }
 
+        public void AlterarNomeTitularConta(string novoNomeTitular)
+        { 
+            this.titular = novoNomeTitular;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {this.numero}, Titular: {this.titular}, Saldo: $ {this.saldo.ToString("N2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
